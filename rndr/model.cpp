71a6fa179e0d731ba46fcb3b2efd16af24612391
K17K47 <lucas_lc.hf@hotmail.com>
2014-11-25 22:34:27 -0200

    Model class placeholder

---
+++ rndr/model.cpp
@@ -0,0 +1,21 @@
+/*
+ * model.cpp
+ *
+ *  Created on: 25/11/2014
+ *      Author: K17K47
+ */
+
+#include <rndr/model.h>
+
+namespace Rndr {
+
+	model::model() {
+		// TODO Stub de construtor gerado automaticamente
+
+	}
+
+	model::~model() {
+		// TODO !CodeTemplates.destructorstub.tododesc!
+	}
+
+} /* namespace Rndr */
---
+++ rndr/model.h
@@ -0,0 +1,21 @@
+/*
+ * model.h
+ *
+ *  Created on: 25/11/2014
+ *      Author: K17K47
+ */
+
+#ifndef MODEL_H_
+#define MODEL_H_
+
+namespace Rndr {
+
+	class model {
+		public:
+			model();
+			~model();
+	};
+
+} /* namespace Rndr */
+
+#endif /* MODEL_H_ */
